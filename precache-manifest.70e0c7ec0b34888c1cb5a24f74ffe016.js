7b9ae5d860766d5d8dd9e90a78833a31d4bf381f
pccoder-2000 <chaturvedipriya998@gmail.com>
2021-05-22 17:31:12 +0530

    Updates

---
+++ precache-manifest.70e0c7ec0b34888c1cb5a24f74ffe016.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b834d7f54cc3a07f990c0d634af9e2f",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "bc48290b48be17affd7b",
+    "url": "/portfolioreact/static/css/main.c31f42af.chunk.css"
+  },
+  {
+    "revision": "619b847c1a5113496b49",
+    "url": "/portfolioreact/static/js/2.a3f8506f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolioreact/static/js/2.a3f8506f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc48290b48be17affd7b",
+    "url": "/portfolioreact/static/js/main.f2b92c66.chunk.js"
+  },
+  {
+    "revision": "ccc82b9744967cf75754",
+    "url": "/portfolioreact/static/js/runtime-main.45fc4603.js"
+  },
+  {
+    "revision": "12786bc79ed7b7c3753201fe4fe4f8ba",
+    "url": "/portfolioreact/static/media/algo.12786bc7.svg"
+  },
+  {
+    "revision": "406f6ef21ed7610dd94e28ec61eb65f7",
+    "url": "/portfolioreact/static/media/api.406f6ef2.svg"
+  },
+  {
+    "revision": "306ae54d171380112cb411e4bde3e136",
+    "url": "/portfolioreact/static/media/backend.306ae54d.svg"
+  },
+  {
+    "revision": "16d20bbc975f5124a8799dd0616c9c7a",
+    "url": "/portfolioreact/static/media/computer.16d20bbc.svg"
+  },
+  {
+    "revision": "cf78973c4b6ba97796aa2dd1bc5b5bea",
+    "url": "/portfolioreact/static/media/email.cf78973c.svg"
+  },
+  {
+    "revision": "c1e769605e4686003967900cb433dc65",
+    "url": "/portfolioreact/static/media/facebook.c1e76960.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/portfolioreact/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/portfolioreact/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "79c07765a3bce94fb118d1d4de1aa0f9",
+    "url": "/portfolioreact/static/media/mightycoder.79c07765.jpg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/portfolioreact/static/media/pin.4b475fb3.svg"
+  },
+  {
+    "revision": "cdfaf911081077c33647e3a43053c99f",
+    "url": "/portfolioreact/static/media/puzzle.cdfaf911.svg"
+  },
+  {
+    "revision": "e2905a883d5f95ef0907cc9adf27ea2d",
+    "url": "/portfolioreact/static/media/python.e2905a88.svg"
+  },
+  {
+    "revision": "9e188349967014b7f52086a714a73c49",
+    "url": "/portfolioreact/static/media/react.9e188349.svg"
+  },
+  {
+    "revision": "3725763121b8e64729ce8bb7f2cd0a74",
+    "url": "/portfolioreact/static/media/repair.37257631.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/portfolioreact/static/media/resume.d41d8cd9.pdf"
+  },
+  {
+    "revision": "7d7f62d33c07ff35485d47e1ba1d583c",
+    "url": "/portfolioreact/static/media/tie.7d7f62d3.svg"
+  }
+]);
